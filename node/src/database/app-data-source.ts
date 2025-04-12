449bfab1251c58b2b651e2aadbb2698ef0563d5c
Marcos Novelli <novehmar23@gmail.com>
2025-04-12 15:05:14 -0300

    Add start-dev script and src entities to TypeORM config for enabling IDE debugging

--- node/src/database/app-data-source.ts
+++ node/src/database/app-data-source.ts
@@ -3,9 +3,12 @@ import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
   type: "sqlite",
   database: "./src/database/spacex.sql",
-  entities: ["./dist/entities/**/*{.ts,.js}"],
+  entities: [
+    "./dist/entities/**/*{.ts,.js}", //For compiled environments
+    "./src/entities/**/*{.ts,.js}", //For development (nodemon)
+  ],
   logging: true,
   synchronize: false,
   migrationsRun: false,
-  migrations: ["./dist/database/migrations/**/*{.ts,.js}"]
+  migrations: ["./dist/database/migrations/**/*{.ts,.js}"],
 });
